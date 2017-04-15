55b92e8726fef6d24b3318a9e49c330b9f7c0750
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-04-14 15:58:01 -1000

    New features added

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 6 */
 const net = require('net');
 const url = process.argv[2];
+const method = process.argv[3];
+const header_hidden = process.argv[4];
 const os = require('os');
 const networkInterfaces = os.networkInterfaces();
 
@@ -8,7 +10,7 @@ const networkInterfaces = os.networkInterfaces();
 const client = net.createConnection({ port: 8080 }, () => {
   const date = new Date().toUTCString();
   const address = networkInterfaces[Object.keys(networkInterfaces)[1]][0].address;
-  const header = `GET ${url} HTTP/1.1
+  const header = `${method} ${url} HTTP/1.1
 Date: ${date}
 Address: ${address}
 User-Agent: Node 7.9.0`;
@@ -16,11 +18,18 @@ User-Agent: Node 7.9.0`;
   client.write(header);
 
   client.on('data', (data) => {
-    console.log(data);
-    const response = data.toString();
-    process.stdout.write(response);
-  });
+    const response = data.toString().split('\n');
 
+    console.log(header_hidden);
+    if (header_hidden === "true"){
+      process.stdout.write(`${response[0]}
+${response[1]}
+${response[2]}
+${response[3]}`);
+    } else if (header_hidden === "false") {
+      process.stdout.write(data.toString());
+    }
+  });
 });
 
 
